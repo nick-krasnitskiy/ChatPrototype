25d5c738ca7e1afcab2cd361a3b4eb33a24eca97
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2024-06-07 15:13:04 +0300

    Initial Commit

---
+++ ChatPrototype/ChatPrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatPrototypeApp.swift
+//  ChatPrototype
+//
+//  Created by Nick Krasnitskiy on 07.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct ChatPrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
