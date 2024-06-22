54138d1f904571d913d5437fbcc31ae5b4581f96
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2024-06-22 23:43:50 +0300

    Add shadows to the bubbles.

--- ChatPrototype/ContentView.swift
+++ ChatPrototype/ContentView.swift
@@ -13,12 +13,15 @@ struct ContentView: View {
             Text("Knock, knock!")
                 .padding()
                 .background(Color.accentColor, in: RoundedRectangle(cornerRadius: 8))
+                .shadow(radius: 50)
             Text("Who's there?")
                 .padding()
                 .background(Color.orange, in: RoundedRectangle(cornerRadius: 8))
+                .shadow(radius: 10)
             Text("It's me - Nick")
                 .padding()
                 .background(Color.cyan, in: RoundedRectangle(cornerRadius: 8))
+                .shadow(radius: 70)
         }
         .padding()
     }
