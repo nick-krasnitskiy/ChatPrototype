34b142ee27de84648c428cd34fc676b6487987f7
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2024-06-22 23:42:04 +0300

    Change the color schemes.

--- ChatPrototype/ContentView.swift
+++ ChatPrototype/ContentView.swift
@@ -12,13 +12,13 @@ struct ContentView: View {
         VStack {
             Text("Knock, knock!")
                 .padding()
-                .background(Color.yellow, in: RoundedRectangle(cornerRadius: 8))
+                .background(Color.accentColor, in: RoundedRectangle(cornerRadius: 8))
             Text("Who's there?")
                 .padding()
-                .background(Color.teal, in: RoundedRectangle(cornerRadius: 8))
+                .background(Color.orange, in: RoundedRectangle(cornerRadius: 8))
             Text("It's me - Nick")
                 .padding()
-                .background(Color.indigo, in: RoundedRectangle(cornerRadius: 8))
+                .background(Color.cyan, in: RoundedRectangle(cornerRadius: 8))
         }
         .padding()
     }
